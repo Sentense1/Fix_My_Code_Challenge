a646bf67b5296ca1d34c619659584326eb83e795
sentense1 <3xsaint@gmail.com>
2023-05-23 04:54:21 -0700

    zero to 4

---
+++ 0x00-challenge/0-fizzbuzz.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" FizzBuzz
+"""
+import sys
+
+
+def fizzbuzz(n):
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
+    """
+    if n < 1:
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        #the code for checking the ' (i % 3) == 0'
+        #and for checking the '(i % 5) == 0'
+        #are already met, that's why it won't get to th code for 
+        #checking the 'fizzbuzz' condition
+        #so we check for the 'fizzbuzz' condition first
+        if (i % 5) == 0 and (i % 3) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
