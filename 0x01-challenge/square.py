b9a6c004bc13d56df32a54d79104789f5c72ace4
sentense1 <3xsaint@gmail.com>
2023-07-13 04:24:46 -0700

    square.p

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,8 +4,6 @@ class Square():
     """
     A class representing a square.
     """
-    width = 0
-    height = 0
 
     def __init__(self, *args, **kwargs):
         """
@@ -15,6 +13,8 @@ class Square():
             *args: Variable length argument list.
             **kwargs: Arbitrary keyword arguments.
         """
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
