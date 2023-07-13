988f811a9ec881f7a0fe6ccc77ef4f63fa941821
sentense1 <3xsaint@gmail.com>
2023-07-13 04:09:55 -0700

    f

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
+
 class Square():
-    
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -20,8 +20,8 @@ class Square():
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
