7c362e3a21236f1bb7aadf63eea75855b45ce67a
sentense1 <3xsaint@gmail.com>
2023-07-13 04:16:45 -0700

    f

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,21 +1,48 @@
 #!/usr/bin/python3
 
 class Square():
+    """
+    A class representing a square.
+    """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a Square object.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """
+        Calculates the area of the square.
+
+        Returns:
+            The area of the square.
+        """
         return self.width * self.height
 
     def Perimeter_Of_My_Square(self):
+        """
+        Calculates the perimeter of the square.
+
+        Returns:
+            The perimeter of the square.
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        Returns a string representation of the square.
+
+        Returns:
+            The string representation of the square.
+        """
         return "{}/{}".format(self.width, self.height)
 
 
